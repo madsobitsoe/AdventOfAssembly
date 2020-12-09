d420fecef742c3d5ddd63ab076344f0f70f6f216
Mads Obitsø <madsobitsoe@gmail.com>
2020-12-09 04:49:11 +0100

    Add tiny version with custom ELF header. 475 bytes is pretty cool

--- day1/build.sh
+++ day1/build.sh
@@ -1,5 +1,6 @@
 # Use nasm and ld
 nasm -f elf64 day1part1.asm && ld day1part1.o -o day1part1
 nasm -f elf64 day1part2.asm && ld day1part2.o -o day1part2
-
+nasm -f bin -o tinyday1part2 tinyday1part2.asm
+chmod +x tinyday1part2
 # Run with ./day1part data.txt - using the dataset from the advent of code challenge
