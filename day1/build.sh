61d559fe34c8f9ea3e9daf79f3e0aa3a00c91fc6
Mads Obitsø <madsobitsoe@gmail.com>
2020-12-09 03:36:12 +0100

    Fix build.sh for day1

--- day1/build.sh
+++ day1/build.sh
@@ -1,4 +1,5 @@
 # Use nasm and ld
-nasm -f elf64 day1part1.asm && ld day1part1.o - day1part1
+nasm -f elf64 day1part1.asm && ld day1part1.o -o day1part1
+nasm -f elf64 day1part2.asm && ld day1part2.o -o day1part2
 
 # Run with ./day1part data.txt - using the dataset from the advent of code challenge
